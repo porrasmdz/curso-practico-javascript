f9436c14f4e081bd5394fa5c6b458fe3d4ecba32
aporrasmdz <aporrasmdz@gmail.com>
2021-10-11 21:02:45 -0500

    Taller de figuras geometricas completo

--- figuras.js
+++ figuras.js
@@ -40,3 +40,73 @@ function pCirculo(radio) {
 function aCirculo(radio) {
     return pi * (radio * radio);
 }
+
+//FINAL DE FIGURAS
+function calcularPerimetroCuadrado()
+{
+    const input = document.getElementById("inputCuadrado");
+    const lado = input.value;
+    const perimetro = perimetroCuadrado(lado);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado()
+{
+    const input = document.getElementById("inputCuadrado");
+    const lado = input.value;
+    const area = areaCuadrado(lado);
+    alert(area);
+}
+
+
+function calcularPerimetroTriangulo()
+{
+    const inputl1 = document.getElementById("inputTrianguloL1");
+    const inputl2 = document.getElementById("inputTrianguloL2");
+    const inputB = document.getElementById("inputTrianguloBase");
+    
+    const lado1 = inputl1.value;
+    const lado2 = inputl2.value;
+    const base = inputB.value;
+
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo()
+{
+    const inputB = document.getElementById("inputTrianguloBase");
+    const inputH = document.getElementById("inputTrianguloAltura");
+    const base = inputB.value;
+    const altura = inputH.value;
+
+    const area = areaTriangulo(base, altura);
+    alert(area);
+}
+
+function calcularPerimetroCirculo()
+{
+    const input = document.getElementById("inputCirculo");
+    const radio = input.value;
+    const perimetro = pCirculo(radio);
+    alert(perimetro);
+}
+
+function calcularAreaCirculo()
+{
+    const input = document.getElementById("inputCirculo");
+    const radio = input.value;
+    const area = aCirculo(radio);
+    alert(area);
+}
+
+function altura(lado1, lado2, base) {
+    if (lado1 == lado2)
+    {
+        const altura = Math.sqrt(((lado2*lado2) - ((base/2) * (base/2))))
+        return altura;
+    }
+    else {
+        alert("Debe dar los datos de un triangulo isoceles");
+    }
+}
